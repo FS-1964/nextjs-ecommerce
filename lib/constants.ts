48dedbdc45f9a55855c26f7438970df5bbbc129d
martin blax <fariborzsaidi@hotmail.com>
2025-02-03 05:46:23 +0100

    constants change

--- lib/constants.ts
+++ lib/constants.ts
@@ -50,5 +50,5 @@ export const AVAILABLE_DELIVERY_DATES = [
   },
 ]
 export const SENDER_NAME = process.env.SENDER_NAME || 'Admin'
-export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'fs.informat@gmail.com'
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'https://www.tengir.site'
